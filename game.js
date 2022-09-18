8cefff3e584eff71e0d2b4369761b68e03d6417b
AmineJml <aminejamal003@gmail.com>
2022-09-18 22:51:50 +0300

    game display score and reset button are not done yet

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 window.onload = () => {
-
+    const nochange_element = document.getElementsByClassName("boundary example")
+    let score = 0;
     let inGame = false
 
     const start_element = document.getElementById("start")
@@ -7,10 +8,9 @@ window.onload = () => {
         inGame = true;
 
 
-        for(let j = 0; j <boundary_element.length; j++){
-            if(inGame)
-            {
-                 boundary_element[j].style.backgroundColor = "#eeeeee"
+        for (let j = 0; j < boundary_element.length; j++) {
+            if (inGame) {
+                boundary_element[j].style.backgroundColor = "#eeeeee"
             }
 
         }
@@ -22,18 +22,58 @@ window.onload = () => {
     const boundary_element = document.getElementsByClassName("boundary")
     for (let i = 0; i < boundary_element.length; i++) {
         boundary_element[i].addEventListener("mouseenter", () => {
+            if (inGame) {
+                console.log("ana l bandura l hamra")
+                for (let j = 0; j < boundary_element.length - 1; j++) {
 
-            console.log("ana l bandura l hamra")
-            for(let j = 0; j <boundary_element.length; j++){
-                if(inGame)
-                {
-                     boundary_element[j].style.backgroundColor = "red"
+                    boundary_element[j].style.backgroundColor = "red"
                 }
+                inGame = false
 
             }
-
-
         })
+    }
+
+    const game_element = document.getElementById("game")
+    for (let i = 0; i < boundary_element.length; i++) {
+        game_element.addEventListener("mouseleave", () => {
+            if (inGame) {
+                console.log("ana l bandura l hamra")
+                for (let j = 0; j < boundary_element.length - 1; j++) {
+
+                    boundary_element[j].style.backgroundColor = "red"
+                }
+                inGame = false
+
+            }
+        })
+    }
+
+    const end_element = document.getElementById("end")
+    end_element.addEventListener("mouseenter", () => {
+        for (let j = 0; j < boundary_element.length - 1; j++) {
+            if (inGame) {
+                boundary_element[j].style.backgroundColor = "green"
+            }
+        }
+  
+        score++;
+        inGame = false
+
+
+
+        console.log("easy peazy lemon squeazy" + score)
+    })
+
+
+    const element_reset = document.getElementById("start")
+    const element_score = document.getElementsByClassName("boundary example")
+    element_reset.onclick =() =>
+    {
+        score = 0;
+        console.log("AAAA")
+        element_score.innerHTML = "Amine"
+
     }
 
 }
