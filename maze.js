2dbe56a5327b3a412e96901a21f8146197e7d198
AmineJml <aminejamal003@gmail.com>
2022-09-18 18:34:50 +0300

    login button is working

---
+++ maze.js
@@ -0,0 +1,20 @@
+// var str = prompt("enter your name");
+// var element = document.getElementById("my_title");
+// console.log(element);
+
+// // element.innerText = str; //innertext is only to add text, innerhtml allows to add html tags + text
+// element.innerHTML = "<i> Hello </i>" +str;
+// element.style.color = "red";
+
+// window.onload = () => { // a functionw ill be called automatrically and can b reused
+//     var x = document.getElementsByClassName("el"); //returns a collection or array
+//     x[0].style.color = "red";
+//     for(var y = 1; y < x.length; y++){
+//         x[y].style.color = "green";
+//     }
+// }
+
+document.getElementById('btn_login').onclick = function() {
+    alert("button was clicked")
+}
+ 
