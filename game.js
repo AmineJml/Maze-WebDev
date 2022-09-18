3835d32df04fbaf9362eaffacd84ba22b0596ca3
AmineJml <aminejamal003@gmail.com>
2022-09-18 22:26:24 +0300

    red is good

--- game.js
+++ game.js
@@ -1,37 +1,39 @@
 window.onload = () => {
 
+    let inGame = false
+
     const start_element = document.getElementById("start")
     start_element.addEventListener("mouseenter", () => {
+        inGame = true;
+
+
+        for(let j = 0; j <boundary_element.length; j++){
+            if(inGame)
+            {
+                 boundary_element[j].style.backgroundColor = "#eeeeee"
+            }
+
+        }
         console.log("ana l bandura l hamra")
 
 
     })
 
     const boundary_element = document.getElementsByClassName("boundary")
-    for(let i = 0; i <boundary_element.length; i++)
-    {
+    for (let i = 0; i < boundary_element.length; i++) {
         boundary_element[i].addEventListener("mouseenter", () => {
-            if(i<1)
-            {
-                console.log("ana l bandura l hamra" )
+
+            console.log("ana l bandura l hamra")
+            for(let j = 0; j <boundary_element.length; j++){
+                if(inGame)
+                {
+                     boundary_element[j].style.backgroundColor = "red"
+                }
 
             }
 
-            else
-            {
-            console.log("gg easy dr get rekt" + i)
-            }
-    
-    
+
         })
     }
 
 }
-
-
-
-//         if(start_element.onmouseenter == true)
-//         {
-//             console.log("SIUUUUUUU")
-//         }
-// }
